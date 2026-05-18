d3a8a2a007a71e1846c05a363d599ad8fe2807f7
ethantuch-netizen <ethan.tuch@gmail.com>
2026-05-18 21:33:15 +0000

    Used copilot from ideas.firstdraft.com to add belongsto associations to response.rb model

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,2 +1,4 @@
 class Response < ApplicationRecord
+  belongs_to :question, required: true, class_name: "Question", foreign_key: "question_id"
+  belongs_to :user, required: true, class_name: "User", foreign_key: "user_id"
 end
