ab0355285ab37ac2fb37ca72c22f890aa9b698ab
ethantuch-netizen <ethan.tuch@gmail.com>
2026-05-18 21:36:36 +0000

    ran rails db:migrate to create tables

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: questions
+#
+#  id                       :bigint           not null, primary key
+#  audio_content_and_answer :string
+#  content                  :text
+#  content_answer           :string
+#  gmat_section             :string
+#  created_at               :datetime         not null
+#  updated_at               :datetime         not null
+#
 class Question < ApplicationRecord
   has_many  :responses, class_name: "Response", foreign_key: "question_id", dependent: :destroy
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: responses
+#
+#  id             :bigint           not null, primary key
+#  correct_result :boolean
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  question_id    :integer
+#  user_id        :integer
+#
 class Response < ApplicationRecord
   belongs_to :question, required: true, class_name: "Question", foreign_key: "question_id"
   belongs_to :user, required: true, class_name: "User", foreign_key: "user_id"
