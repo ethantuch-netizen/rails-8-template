5c3d1233df984293cd47ab356298ccccd12a7f6a
ethantuch-netizen <ethan.tuch@gmail.com>
2026-05-18 21:34:40 +0000

    used copilot from ideas.firstdraft.com to add has_many association to user.rb model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many  :responses, class_name: "Response", foreign_key: "user_id", dependent: :destroy
 end
