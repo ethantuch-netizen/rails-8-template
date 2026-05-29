03c6b9a8af15f63d3a26eb00469ac6172795edbd
ethantuch-netizen <ethan.tuch@gmail.com>
2026-05-29 19:55:30 +0000

    removed route from routes to put questions onto production database because I couldn't get it to work

--- config/routes.rb
+++ config/routes.rb
@@ -12,32 +12,4 @@ Rails.application.routes.draw do
 
   get("/start_audio", { :controller => "responses", :action => "audio" })
 
-  require "digest"
-
-  Rails.application.routes.draw do
-    rails_db_username = ENV["RAILS_DB_USERNAME"]
-    rails_db_password = ENV["RAILS_DB_PASSWORD"]
-
-    if Rails.env.production? && rails_db_username.present? && rails_db_password.present?
-      protected_rails_db = Rack::Auth::Basic.new(RailsDb::Engine) do |username, password|
-        username_matches = ActiveSupport::SecurityUtils.secure_compare(
-          Digest::SHA256.hexdigest(username.to_s),
-          Digest::SHA256.hexdigest(rails_db_username.to_s)
-        )
-
-        password_matches = ActiveSupport::SecurityUtils.secure_compare(
-          Digest::SHA256.hexdigest(password.to_s),
-          Digest::SHA256.hexdigest(rails_db_password.to_s)
-        )
-
-        username_matches & password_matches
-      end
-
-      mount(protected_rails_db => "/rails/db")
-    elsif !Rails.env.production?
-      mount(RailsDb::Engine => "/rails/db")
-    end
-
-    # your other routes go here
-  end
 end
