77fc61896fa4be77197e7c1130fc5bc408d3816e
ethantuch-netizen <ethan.tuch@gmail.com>
2026-05-29 16:33:43 +0000

    removed unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,48 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Routes for the Response resource:
+
   
-  # CREATE
-  post("/insert_response", { :controller => "responses", :action => "create" })
-
-  # READ
-  get("/responses", { :controller => "responses", :action => "index" })
-  #root "responses#index"
-
-  get("/responses/:path_id", { :controller => "responses", :action => "show" })
-
-  # UPDATE
-
-  post("/modify_response/:path_id", { :controller => "responses", :action => "update" })
-
-  # DELETE
-  get("/delete_response/:path_id", { :controller => "responses", :action => "destroy" })
-
-  #------------------------------
-
-  # Routes for the Question resource:
-
-  # CREATE
-  post("/insert_question", { :controller => "questions", :action => "create" })
-
-  # READ
-  get("/questions", { :controller => "questions", :action => "index" })
-
-  get("/questions/:path_id", { :controller => "questions", :action => "show" })
-
-  # UPDATE
-
-  post("/modify_question/:path_id", { :controller => "questions", :action => "update" })
-
-  # DELETE
-  get("/delete_question/:path_id", { :controller => "questions", :action => "destroy" })
-
-  #------------------------------
-
-  # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
-  # get("/your_first_screen", { :controller => "pages", :action => "first" })
 
   get("/", { :controller => "questions", :action => "home" })
+  
+  get("/questions", { :controller => "questions", :action => "index" })
   
   get("/start_quiz", { :controller => "responses", :action => "flashcards" })
   post("/build_study_set", { :controller => "responses", :action => "build_study_set" })
